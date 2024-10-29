a40427be88c6d0bae1242bde854716a273826fdc
Edhain <manasdps@gmail.com>
2024-10-29 22:45:02 +0530

    Adding initial javascript  learning files

---
+++ primitives.js
@@ -0,0 +1,5 @@
+let name = 'Manas';
+let age = 30;
+let isApproved = false;
+let firstName;
+let lastName = null;
---
+++ variables.js
@@ -0,0 +1,6 @@
+let userName = 'Manas';
+console.log(userName);
+
+const interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate)
